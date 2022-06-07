6c6a1f2a8efa5c43095d15ce02d3baf9b6aa3880
ethanrmorris <ethan.morris33@gmail.com>
2022-06-07 16:58:42 -0500

    1 file modified, 1 added

--- components/Layout.js
+++ components/Layout.js
@@ -1,34 +1,32 @@
 import Link from 'next/link';
 
+import styles from '../styles/Layout.module.css';
+
 export default function Layout({ children }) {
   return (
-    <>
-      <div>
+    <div className={styles.gridLayout}>
+      <header className={styles.header}>Nav Bar</header>
+      <nav className={styles.nav}>
         <Link href="/">
           <a>Home</a>
         </Link>
-
         <Link href="/trades">
           <a>Trades</a>
         </Link>
-
         <Link href="/stats">
           <a>Stats</a>
         </Link>
-
         <Link href="/owners">
           <a>Owners</a>
         </Link>
-
         <Link href="/players">
           <a>Players</a>
         </Link>
-
         <Link href="/supabase">
           <a>Supabase</a>
         </Link>
-      </div>
-      <div>{children}</div>
-    </>
+      </nav>
+      <main className={styles.main}>{children}</main>
+    </div>
   );
 }
